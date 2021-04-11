c5d17dee81ae9636e9e16f4befe2e7bfb6abfd97
Cesar Kennedy Rousseau Arzapalo Caldas <cesar.arzapalo@unmsm.edu.pe>
2021-04-11 08:38:31 -0500

    añadiendo variables en los enviroments de produccionpara presentacion de avance

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  backendCrudURL:"localhost:3000",
+  firebase: { 
+    apiKey: "AIzaSyCBg14wxYif5BTjkMkmY--BrYAtqJAtgAQ",
+    authDomain: "emark-af49a.firebaseapp.com",
+    projectId: "emark-af49a",
+    storageBucket: "emark-af49a.appspot.com",
+    messagingSenderId: "815800013340",
+    appId: "1:815800013340:web:865f7bf3b8302393f445d9",
+    measurementId: "G-RESV87CL5Z"
+  }
 };
