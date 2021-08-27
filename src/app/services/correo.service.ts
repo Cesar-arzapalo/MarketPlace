01431059fc53374705ca4d6efb7cd4830ec580d5
RousMeiki <rousmeiki@gmail.com>
2021-08-26 19:44:08 -0500

    aniadiendo headers de cors 3

--- src/app/services/correo.service.ts
+++ src/app/services/correo.service.ts
@@ -10,8 +10,8 @@ export class CorreoService {
   headers:HttpHeaders = new HttpHeaders ({
     'Access-Control-Allow-Credentials' : 'true',
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',    
     "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe",
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS'
   });
   constructor(private http:HttpClient) { 
     
